aa382713465848943bfa6f74d95cd0dd23dd30ab
jrodrigovaldez <rodrigo@fpuna.edu.py>
2022-09-09 18:10:48 -0400

    Creación de los managers para los modelos

--- DirCom/applications/users/models.py
+++ DirCom/applications/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# from .managers import UserManager
+from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -17,20 +17,23 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     VINC_CHOICES = (("1", "Contratado"), ("2", "Permanente"),)
 
-    username = models.CharField("nombre de usuario", max_length=50, unique=True)
-    first_name = models.CharField("nombres", max_length=150)
-    last_name = models.CharField("apellidos", max_length=150)
-    gov_id = models.CharField("documento de identidad", max_length=10)
-    email = models.EmailField("correo electrónico", max_length=254, unique=True)
-    city = models.CharField("ciudad", max_length=30)
-    phone = models.CharField("número de teléfono", max_length=30)
-    area = models.CharField("dependencia", max_length=150)
-    vinc_type = models.CharField("tipo de vinculación", choices=VINC_CHOICES, max_length=1)
+    # campos obligatorios
+    username = models.CharField("nombre de usuario", max_length=50, unique=True) 
+    gov_id = models.CharField("documento de identidad", max_length=10, unique=True) 
+    email = models.EmailField("correo electrónico", max_length=254, unique=True) 
+    # campos extras
+    first_name = models.CharField("nombres", blank=True, null=True, max_length=150)
+    last_name = models.CharField("apellidos", blank=True, null=True, max_length=150)
+    city = models.CharField("ciudad", blank=True, null=True,  max_length=30)
+    phone = models.CharField("número de teléfono", blank=True, null=True, max_length=30)
+    area = models.CharField("dependencia", blank=True, null=True, max_length=150)
+    vinc_type = models.CharField("tipo de vinculación", blank=True, null=True, choices=VINC_CHOICES, max_length=1)
     is_staff = models.BooleanField("staff", default=False)
 
+    # convierte el campo username en obligatorio por default
     USERNAME_FIELD = "username"
 
     # para que la terminal nos pida estos datos al crear un superuser
-    REQUIRED_FIELDS = ['email', 'full_name']
+    REQUIRED_FIELDS = ['email', 'gov_id']
 
-    # objects = UserManager()
+    objects = UserManager()
