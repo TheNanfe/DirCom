42a5f43c2c6da4b9ef2c108d7bb1c174d9001202
jrodrigovaldez <rodrigo@fpuna.edu.py>
2022-09-11 15:52:35 -0400

    Implementación del modelo Persona

--- DirCom/applications/users/models.py
+++ DirCom/applications/users/models.py
@@ -3,6 +3,30 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 from .managers import UserManager
 
+class Persona(models.Model):
+    """
+        Clase para generar mi modelo de Persona que es el modelo
+        previo a crear un usuario, en este modelo se guardan todos
+        los datos personales
+    """
+
+    VINC_CHOICES = (("1", "Contratado"), ("2", "Permanente"),)
+
+    gov_id = models.CharField("documento de identidad", max_length=50, unique=True) 
+    email = models.EmailField("correo electrónico", max_length=254, unique=True) 
+    first_name = models.CharField("nombres", blank=True, null=True, max_length=150)
+    last_name = models.CharField("apellidos", blank=True, null=True, max_length=150)
+    city = models.CharField("ciudad", blank=True, null=True,  max_length=30)
+    phone = models.CharField("número de teléfono", blank=True, null=True, max_length=30)
+    area = models.CharField("dependencia", blank=True, null=True, max_length=150)
+    vinc_type = models.CharField("tipo de vinculación", blank=True, null=True, choices=VINC_CHOICES, max_length=1)
+
+    class Meta:
+        verbose_name = "persona"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ 
@@ -15,26 +39,15 @@ class User(AbstractBaseUser, PermissionsMixin):
         -is_admin
     """
 
-    VINC_CHOICES = (("1", "Contratado"), ("2", "Permanente"),)
-
-    # campos obligatorios
-    username = models.CharField("nombre de usuario", max_length=50, unique=True) 
-    gov_id = models.CharField("documento de identidad", max_length=50, unique=True) 
-    email = models.EmailField("correo electrónico", max_length=254, unique=True) 
-    # campos extras
-    first_name = models.CharField("nombres", blank=True, null=True, max_length=150)
-    last_name = models.CharField("apellidos", blank=True, null=True, max_length=150)
-    city = models.CharField("ciudad", blank=True, null=True,  max_length=30)
-    phone = models.CharField("número de teléfono", blank=True, null=True, max_length=30)
-    area = models.CharField("dependencia", blank=True, null=True, max_length=150)
-    vinc_type = models.CharField("tipo de vinculación", blank=True, null=True, choices=VINC_CHOICES, max_length=1)
+    username = models.CharField("nombre de usuario", max_length=50, unique=True)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE, related_name="user")
     is_staff = models.BooleanField("staff", default=False)
 
     # convierte el campo username en obligatorio por default
     USERNAME_FIELD = "username"
 
     # para que la terminal nos pida estos datos al crear un superuser
-    REQUIRED_FIELDS = ['email', 'gov_id']
+    REQUIRED_FIELDS = ['persona']
 
     objects = UserManager()
 
--- README.md
+++ README.md
@@ -14,4 +14,8 @@ Repositorio Proyecto 2. FPUNA, 2022. HL, SDV, RV
 
 4. Correr el comando `python manage.py makemigrations` y luego `python manage.py migrate`
 
-5. Listo!
+5. Para crear tu usuario es necesario primero: crear una instancia de Persona y luego podrás crear tu superadmin, así que primero corre el comando `python manage.py dircom <tu_documento_de_identidad> <tu_email>`. Ej: `python manage.py dircom 3214532 juan@gmail.com`. De esta manera creas el primer usuario del sistema, así que tiene el id=1
+
+6. Ahora, para crear un superuser debes correr el comando `python manage.py createsuperuser`, y te pedirá un nombre de usuario, un id de persona, que es el que acabas de crear (1) y una contraseña.
+
+7. Listo!
