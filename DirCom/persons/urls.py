37d2f8bd9bbf446bd6aac56bb59eb2728bf0bea5
TheNanfe <nanrelo@gmail.com>
2022-09-11 23:41:22 -0400

    #Restableciendo rama main

--- DirCom/persons/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list_persons, name='person_list'),
-    path('<str:pk>', views.person_details, name='person_details'),
-    path('create/', views.create_person, name='create_person')
-]
--- DirCom/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list_users, name='user_list'),
-    path('<int:pk>', views.get_user, name='user_details'),
-    path('create/', views.create_user, name='create_user'),
-    path('delete/<int:pk>', views.delete_user, name='delete_user'),
-]
