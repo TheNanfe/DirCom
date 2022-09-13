526f05da0bd4f07f6c3fdda4b5da03aa46ac054a
jrodrigovaldez <rodrigo@fpuna.edu.py>
2022-09-13 15:37:59 -0400

    Restricción de las vistas para usuarios

--- DirCom/applications/core/views.py
+++ DirCom/applications/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 
 # Create your views here.
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "core/home.html"
+    login_url = reverse_lazy("users_app:login")
