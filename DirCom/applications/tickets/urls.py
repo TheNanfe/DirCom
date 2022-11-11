f2276f1c58cd9b506fbc9eb9f53469ad5314a4ab
jrodrigovaldez <rodrigo@fpuna.edu.py>
2022-11-11 00:36:37 -0300

    Desarrollo de la página de reportes

--- DirCom/applications/tickets/urls.py
+++ DirCom/applications/tickets/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.AllTicketsView.as_view(), name="all"),
     path("csv/", views.export_tickets_csv, name="csv"),
     path("exportar/", views.export_tickets, name="export"),
+    path("reportes/", views.tickets_reports, name="reports"),
     path("nuevo/", views.CreateTicketView.as_view(), name="create"),
     path("thanks/", views.ThanksView.as_view(), name="thanks"),
     path("editar/<pk>/", views.EditTicketView.as_view(), name="edit"),
--- DirCom/applications/tickets/views/export_tickets.py
+++ DirCom/applications/tickets/views/export_tickets.py
@@ -1,12 +1,14 @@
+import csv
+import io
+import json
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, FileResponse
-import io
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
-from datetime import datetime
-import csv
+from datetime import date, datetime, timedelta
+from django.utils import formats
 
 from applications.tickets.models import Ticket, Category
 from applications.users.models import User
@@ -15,7 +17,7 @@ from applications.users.models import User
 def export_tickets(request):
     agents = User.objects.filter(role=2)
     categories = Category.objects.all()
-    today = datetime.today().strftime('%Y-%m-%d')
+    today = datetime.today().strftime("%Y-%m-%d")
     context = {
         "agents": agents,
         "categories": categories,
@@ -44,7 +46,7 @@ def export_tickets_pdf(request):
         lines.append(str(ticket.agent))
         lines.append(ticket.content)
         lines.append(str(ticket.category))
-        #lines.append(ticket.created_at)
+        # lines.append(ticket.created_at)
         lines.append(" ")
 
     for line in lines:
@@ -59,8 +61,8 @@ def export_tickets_pdf(request):
 
 
 def export_tickets_csv(request):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=tickets.csv'
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = "attachment; filename=tickets.csv"
 
     writer = csv.writer(response)
 
@@ -89,36 +91,109 @@ def export_tickets_csv(request):
         tickets = tickets.filter(created_at__range=(dateFrom, dateTo))
 
     # agregar los encabezados de las columnas
-    writer.writerow([
-        'Autor',
-        'Email',
-        'Título',
-        'Estado',
-        'Urgencia',
-        'Categoría',
-        'Agente',
-        'Fecha de creación',
-        'Ver ticket',
-        'Motivo de rechazo',
-    ])
+    writer.writerow(
+        [
+            "Autor",
+            "Email",
+            "Título",
+            "Estado",
+            "Urgencia",
+            "Categoría",
+            "Agente",
+            "Fecha de creación",
+            "Ver ticket",
+            "Motivo de rechazo",
+        ]
+    )
 
     for ticket in tickets:
-        writer.writerow([
-            str(ticket.user.persona),
-            ticket.email,
-            ticket.title,
-            ticket.get_status_display(),
-            ticket.get_urgency_display(),
-            ticket.category.title,
-            ticket.agent.persona if ticket.agent else "No asignado",
-            ticket.created_at.strftime("%d-%m-%Y %H:%M"),
-            request.build_absolute_uri(reverse('tickets_app:detail',  kwargs={"pk": ticket.id})),
-            ticket.rejection_message
-        ])
+        writer.writerow(
+            [
+                str(ticket.user.persona),
+                ticket.email,
+                ticket.title,
+                ticket.get_status_display(),
+                ticket.get_urgency_display(),
+                ticket.category.title,
+                ticket.agent.persona if ticket.agent else "No asignado",
+                ticket.created_at.strftime("%d-%m-%Y %H:%M"),
+                request.build_absolute_uri(
+                    reverse("tickets_app:detail", kwargs={"pk": ticket.id})
+                ),
+                ticket.rejection_message,
+            ]
+        )
 
     return response
 
 
+def formatDate(d):
+    return "Fecha_" + formats.date_format(d, "d_m_Y")
+
+
 def tickets_reports(request):
-    context = {"id": 1}
-    render(request, "tickets/reports.html", context)
+    tickets = Ticket.objects.all()
+
+    form_has_date_from = False
+    if request.GET.get("from", ""):
+        form_has_date_from = True
+
+    form_has_date_to = False
+    if request.GET.get("to", ""):
+        form_has_date_to = True
+
+    # filtros
+    status = request.GET.get("status", "")
+    agent = request.GET.get("agent", "")
+    urgency = request.GET.get("urgency", "")
+    category = request.GET.get("category", "")
+    dateFrom = request.GET.get("from", datetime.today() - timedelta(days=7))
+    dateTo = request.GET.get("to", datetime.today())
+
+    if status:
+        tickets = tickets.filter(status=status)
+    if agent:
+        agent = User.objects.get(username=agent)
+        tickets = tickets.filter(agent=agent)
+    if urgency:
+        tickets = tickets.filter(urgency=urgency)
+    if category:
+        tickets = tickets.filter(category=category)
+    if dateFrom:
+        if form_has_date_from:
+            dateFrom = f"{dateFrom} 00:00"
+        if form_has_date_to:
+            dateTo = f"{dateTo} 00:00" if dateTo else datetime.today()
+        tickets = tickets.filter(created_at__range=(dateFrom, dateTo))
+
+    result = {}
+    if form_has_date_from:
+        delta = datetime.strptime(dateTo, "%Y-%m-%d %H:%M") - datetime.strptime(
+            dateFrom, "%Y-%m-%d %H:%M"
+        )
+    else:
+        delta = dateTo - dateFrom
+
+    for i in range(delta.days + 1):
+        day = (
+            datetime.strptime(dateFrom, "%Y-%m-%d %H:%M") + timedelta(days=i)
+            if form_has_date_from
+            else dateFrom + timedelta(days=i)
+        )
+        result[formatDate(day)] = 0
+    for ticket in tickets:
+        result[formatDate(ticket.created_at)] += 1
+
+    agents = User.objects.filter(role=2)
+    categories = Category.objects.all()
+    today = datetime.today().strftime("%Y-%m-%d")
+    default_from = (datetime.today() - timedelta(days=7)).strftime("%Y-%m-%d")
+
+    context = {
+        "result": json.dumps(result), 
+        "today": today,
+        "default_from": default_from,
+        "agents": agents,
+        "categories": categories
+    }
+    return render(request, "tickets/reports.html", context)
